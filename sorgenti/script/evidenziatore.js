e9f33d22c1ee924b2934120293d64b53a675044b
edoardo <edoardo@aspix.it>
2018-06-25 14:59:22 +0200

    sistemato schema colori per le sezioni, i programmi sono tutti contenuti in pre/code

--- sorgenti/script/evidenziatore.js
+++ sorgenti/script/evidenziatore.js
@@ -1,15 +1,13 @@
 function evidenzia(nome){
 	cont=nome;
-	base = document.getElementById("areaEvidenziabile").getElementsByTagName("div");
+	base = document.getElementById("areaEvidenziabile").getElementsByTagName("code");
 	for (var i = 0; i < base.length; i++) {
 		nodo = base[i];
 		applicabile = nodo.getAttribute("data-riguarda"); 
 		if(applicabile!=null && applicabile.search(cont)!=-1){
 			nodo.style.backgroundColor = 'yellow';
-			nodo.style.padding = '3px';
 		}else{
 			nodo.style.backgroundColor = 'transparent';
-			nodo.style.padding = '0px';
 		}
 	}	
 	return false;
