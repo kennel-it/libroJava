f66cd335c24800edb3cf0d6b7b675bef06550526
Edoardo Panfili <edoardopa@gmail.com>
2019-08-03 11:59:52 +0200

    aggiornati link alla documentazione riattivata scelta ide

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
   - creare cartella "compilati"
   - click con il destro su "build.xml" e run as Ant build
   
+# debug in locale
+Se firefox solleva eccezioni per richieste cors utilizzando i file (senza passare per server web)
+va impostata a false la proprietà security.fileuri.strict_origin_policy (accessibile via about:config)
+come reference: http://kb.mozillazine.org/Security.fileuri.strict_origin_policy
+  
 # come scrivere i link per l'indice
 - titlepage.xhtml?+interfacce polimorfismo archiviareOggetti
   in questo tutti gli altri nodi vengono sbiancati
--- sorgenti/script/test.js
+++ sorgenti/script/test.js
@@ -6,18 +6,3 @@ function sbagliata(obj){
         obj.classList.add("spiega");
         obj.classList.add("rispostaSbagliata");
 }
-function sistemaIde(){
-	var nome=localStorage.getItem("ide");
-	var altri={"eclipse":".netbeans", "netbeans":".eclipse"};
-	if(nome!=null){
-		console.log(nome);
-		console.log("##"+altri[nome]);
-		elementList = document.querySelectorAll(altri[nome]);
-		for(i=0; i<elementList.length; i++){
-			console.log("  modificato "+elementList[i]);
-			elementList[i].className += " IDE_inutile";	
-		}
-	}else{
-		console.log("nessuna prefernza sull'IDE");
-	}
-}
