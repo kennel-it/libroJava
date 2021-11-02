18eeb8781fe88d710c04f9a6379b1bf902b85afb
Edoardo Panfili <edoardopa@gmail.com>
2021-11-02 20:57:53 +0100

    spunto per esercizio

---
+++ src/it/aspix/librojava/esercizi/ide/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * - installare sulla scrivania una precisa versione di Eclipse - JDK- JavaFX 
+ *   con uno workspace a parte senza interferire con l'altro
+ */
+package it.aspix.librojava.esercizi.ide;
