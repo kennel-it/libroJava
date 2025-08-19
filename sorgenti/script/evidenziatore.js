47d583429db323c8edc1b25ade29723995b36bd0
Edoardo Panfili <edoardo.panfili@iisgubbio.edu.it>
2025-08-19 18:53:01 +0200

    seria ripulita al foglio di stile

--- sorgenti/script/evidenziatore.js
+++ sorgenti/script/evidenziatore.js
@@ -1,6 +1,6 @@
 function evidenzia(nome){
 	cont=nome;
-	base = document.getElementById("areaEvidenziabile").getElementsByTagName("code");
+	base = document.getElementById("areaEvidenziabile").querySelectorAll("span.row");
 	for (var i = 0; i < base.length; i++) {
 		nodo = base[i];
 		applicabile = nodo.getAttribute("data-riguarda"); 
